79cabb3c0fc0c78fdee7c125b64f6beeab358e34
Nazif Barassounon <nazifbarassounon@gmail.com>
2025-05-19 18:02:31 +0100

    add basic layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,24 @@
 <script lang="ts">
 	import '../app.css';
+	import { ModeWatcher } from 'mode-watcher';
+
+	import { Button } from '$lib/components/ui/button';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<ModeWatcher />
+
+<div class="mx-auto grid w-full max-w-4xl grid-cols-[30%_1fr] gap-4 px-2 py-4">
+	<aside>
+		<Button>Créer un cours +</Button>
+		<ul class="grid gap-2 py-4">
+			<li><a href="/#">Cour 1</a></li>
+			<li><a href="/#">Cour 2</a></li>
+			<li><a href="/#">Cour 3</a></li>
+		</ul>
+	</aside>
+	<main>
+		{@render children()}
+	</main>
+</div>
