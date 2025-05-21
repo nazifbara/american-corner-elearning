00d78b17f0eb60c19c1b46d6982659ea166311c9
Nazif Barassounon <nazifbarassounon@gmail.com>
2025-05-21 20:52:34 +0100

    add api route for meeting creation

---
+++ src/routes/call/[slug]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+	import Zoom from './Zoom.svelte';
+
+	const slug = page.url.pathname.split('/')[2];
+
+	onMount(async () => {
+		if (browser) {
+			const response = await fetch('/api/meeting', {
+				method: 'POST',
+				body: JSON.stringify({ topic: slug })
+			});
+			console.log(await response.json());
+		}
+	});
+</script>
+
+<div>
+	<h1>Session: {slug}</h1>
+	<!-- <Zoom /> -->
+</div>
---
+++ src/routes/call/[slug]/Zoom.svelte
@@ -0,0 +1,21 @@
+<!-- <script lang="ts">
+	import { onMount } from 'svelte';
+	import ZoomVideo, { type VideoPlayer, VideoQuality } from '@zoom/videosdk';
+
+	type Props = {
+		JWT: string;
+		slug: string;
+	};
+	const { JWT, slug }: Props = $props();
+
+	const username = `User-${String(new Date().getTime()).slice(6)}`;
+	const client = ZoomVideo.createClient();
+	let videoContainer: HTMLElement | undefined = $state(undefined);
+	let inSession = $state(false);
+	let audioMuted = $state(false);
+	let videoMuted = $state(false);
+
+	onMount(async () => {
+		await client.init('en-US', 'Global', { patchJsMedia: true });
+	});
+</script> -->
