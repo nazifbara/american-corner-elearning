77fe79cf57c0a67e780b2604b685236f9011981e
Nazif Barassounon <nazifbarassounon@gmail.com>
2025-07-29 16:10:07 +0100

    allow user to add resources

--- src/lib/firebase/resources.ts
+++ src/lib/firebase/resources.ts
@@ -1,3 +1,6 @@
+import { collection, addDoc } from 'firebase/firestore';
+import { firestore } from '.';
+
 export type Resource = {
 	id: string;
 	creatorId: string;
@@ -8,3 +11,29 @@ export type Resource = {
 	createdAt: Date;
 	updatedAt: Date;
 };
+
+export type CreateResource = {
+	title: string;
+	url: string;
+	creatorId: string;
+};
+
+const resourcesRef = collection(firestore, 'resources');
+
+export async function addResource(data: CreateResource): Promise<Resource> {
+	const docRef = await addDoc(resourcesRef, {
+		...data,
+		tags: [],
+		type: 'youtube-video',
+		createdAt: new Date(),
+		updatedAt: new Date()
+	});
+	return {
+		id: docRef.id,
+		...data,
+		tags: [],
+		type: 'youtube-video',
+		createdAt: new Date(),
+		updatedAt: new Date()
+	};
+}
--- src/routes/app/admin/resources/+page.svelte
+++ src/routes/app/admin/resources/+page.svelte
@@ -1,31 +1,54 @@
 <script lang="ts">
-	import { PlusIcon } from '@lucide/svelte';
+	import { PlusIcon, Loader2Icon } from '@lucide/svelte';
+	import { toast } from 'svelte-sonner';
+
 	import { Input } from '$lib/components/ui/input';
 	import { Button } from '$lib/components/ui/button';
+	import { EntityList } from '$lib/state/entity-list-state.svelte';
+	import { addResource } from '$lib/firebase/resources';
+	import { authState } from '$lib/state/shared.svelte';
 
-	let label = $state('');
+	let title = $state('');
 	let url = $state('');
 
-	function handleSubmit(event: Event) {
+	const resourceList = new EntityList({
+		addFn: () => addResource({ title, url, creatorId: authState.user!.uid })
+	});
+
+	async function handleSubmit(event: Event) {
 		event.preventDefault();
+		if (title && url && !resourceList.adding) {
+			await resourceList.add();
+			title = '';
+			url = '';
+			toast.success('Ressource ajoutée avec succès');
+		}
 	}
 </script>
 
 <div class="mx-auto grid max-w-4xl">
 	<h1 class="mb-4 text-2xl">Gestion des ressources</h1>
-	<form class="flex items-center gap-2">
+	<form class="flex items-center gap-2" onsubmit={handleSubmit}>
 		<Input
 			type="text"
-			bind:value={label}
+			bind:value={title}
 			required
 			placeholder="Donnez un titre à la video youtube..."
+			disabled={resourceList.adding}
 		/>
 		<Input
 			type="url"
 			bind:value={url}
 			required
 			placeholder="Ajouter l'URL de la video youtube..."
+			disabled={resourceList.adding}
 		/>
-		<Button size="icon" type="submit" aria-label="Ajouter"><PlusIcon /></Button>
+		<Button size="icon" type="submit" aria-label="Ajouter">
+			{#if resourceList.adding}
+				<Loader2Icon size={20} class="animate-spin" />
+			{:else}
+				<PlusIcon size={20} />
+			{/if}
+		</Button>
 	</form>
 </div>
